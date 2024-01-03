b251afc3afb8596224b3f7eea1f103620b0d012c
Aman243y <93468038+Aman243y@users.noreply.github.com>
2024-01-03 16:13:39 +0530

    Add files via upload

---
+++ config.sql
@@ -0,0 +1,4 @@
+create database config;
+use config;
+create table student(id int auto_increment primary key,first_name varchar(200),last_name varchar(200),email varchar(200));
+select * from student; 
